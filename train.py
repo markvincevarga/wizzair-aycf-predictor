691797ad85c2857ffcbd741672fea364ada17408
Márk Vince Varga <mark.vince.varga@gmail.com>
2025-12-20 14:34:35 +0100

    use time-based train test split

--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 # %%
 import data.training
-from sklearn.model_selection import train_test_split
+from data.split import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 from xgboost import XGBRegressor
 import matplotlib.pyplot as plt
@@ -14,17 +14,14 @@ df.tail()
 
 # Initially we remove the departure and destination altogether, later
 # versions may use use them as features or we may have one model for each
-training_df = df.drop(
-    columns=["id", "availability_start",  "departure_from", "departure_from_country", "departure_to", "departure_to_country"], errors="ignore"
-)
-# training_df = training_df.drop(columns=["occurs_lag_1", "occurs_lag_2", "occurs_lag_3", "occurs_mean_prev_7d", "occurs_mean_prev_14d"], errors="ignore")
-X = training_df.drop(columns=["occurs"], errors="ignore")
-y = training_df["occurs"]
+DROP_COLS = ["id", "availability_start", "departure_from", "departure_from_country", "departure_to", "departure_to_country"]
+# DROP_COLS += ["occurs_lag_1", "occurs_lag_2", "occurs_lag_3", "occurs_mean_prev_7d", "occurs_mean_prev_14d"]
 
-# Train-test split (80% train, 20% test)
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
-)
+# Time-based train-test split (80% train, 20% test)
+X_train, X_test, y_train, y_test = train_test_split(df)
+X_train = X_train.drop(columns=DROP_COLS, errors="ignore")
+X_test = X_test.drop(columns=DROP_COLS, errors="ignore")
+X = df.drop(columns=DROP_COLS + ["occurs"], errors="ignore")
 
 print(f"Training set: {len(X_train)} samples")
 print(f"Test set: {len(X_test)} samples")
